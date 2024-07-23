9891ce4fddf1b33b52185c805b7ebf46a8929f3c
priyanka807 <priyankacoder11@gmail.com>
2024-07-23 18:19:12 +0530

    update productdetails page

--- index.js
+++ index.js
@@ -11,4 +11,29 @@
 
 
 
+const specification = document.getElementById('specification')
+const features = document.getElementById('features')
+const more_products = document.getElementById('more_products')
+const view_complete_details = document.getElementById('view_complete_details')
+
+
+let isShowing = false
+view_complete_details.addEventListener('click',()=>{
+    if(!isShowing){
+        specification.style.display = "block"  ;
+        features.style.display = "block"   ;
+        more_products.style.display = "block"   ;
+        isShowing = true
+    }else{
+        specification.style.display = "none"  ;
+        features.style.display = "none"   ;
+        more_products.style.display = "none"   ;
+        isShowing = false
+    }
+  
+    // console.log(specification,features,get_quote,view_complete_details )
+
+})
+
+
 
